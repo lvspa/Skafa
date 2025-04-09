5685c667869dd3448771f26bdcbc286fe2651867
lvspa <166564473+lvspa@users.noreply.github.com>
2025-04-09 14:40:56 -0300

    conector editado

--- core/ConexaoBD.php
+++ core/ConexaoBD.php
@@ -1,23 +1,30 @@
 <?php
 class Database {
+    private static $conn;
     public static function connect() {
         if (self::$conn === null) {
             $config= require 'config.php';
             try {
                 self::$conn = new PDO(
-                    "mysql:host={$config['host']};dbname= {$config['dbname']} ;charset=utf8,
-                    {$config['username']},
-                    {$config['password']}"
+                    "mysql:host={$config['host']};dbname= {$config['dbname']} ;charset-utf8,"
+                    ,$config['username'],
+                    $config['password']
+
                 );
+                echo "Conected :D ";
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die('Erro de conexão: ' . $e->getMessage());
+                echo "Error connecting :(";
             }
         }
         return self::$conn;
     }
 
-    public static function disconnect() {
+    public  function disconnect() {
         self::$conn = null;
     }
 }
+require_once 'ConexaoBD.php';
+$conectar=Database::connect();
+$desconectar=Database::disconnect();
