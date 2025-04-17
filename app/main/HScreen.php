236addd6973b4964b9f433e5a33f062e1dd9740c
lvspa <166564473+lvspa@users.noreply.github.com>
2025-04-17 13:40:20 -0300

    Insercao de metodos e captura de dados

--- app/main/HScreen.php
+++ app/main/HScreen.php
@@ -1,3 +1,31 @@
+<?php
+$conn=Database::connect();
+$email=$_POST["email"];
+$senha=$_POST["senha"];
+
+// Consulta ao banco
+$stmt = $conn->prepare("SELECT * FROM user WHERE email = ? AND senha = ?");
+$stmt->execute([$email, $senha]);
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Verifica se encontrou um usu�rio
+if ($user) {
+    echo "<script>alert('Login realizado com sucesso!');</script>";
+    echo "<script>window.location.href = 'pagina_principal.php';</script>";
+} else {
+    echo "<script>alert('Nome ou senha inv�lidos.');</script>";
+}
+
+
+
+
+
+
+
+?>
+
+
 <html></html><!DOCTYPE html>
 <html lang="pt-BR">
 <link rel="stylesheet" href="homestyle.css">
@@ -15,18 +43,18 @@
     </div>
 
     <div class="campo_input estilo_input">
-        <label for="matr">Email</label>
+        <label for="matr" name="email">Email</label>
         <input type="matr" id="matr" />
     </div>
     <div class="campo_input estilo_input" >
-        <label for="password"><bolt>Senha</bolt></label>
+        <label for="password" name="senha"><bolt>Senha</bolt></label>
         <input type="password" id="password" />
     </div>
-    <button  id="ibotao" class="botao">
+    <button id="ibotao" class="botao">
         Entrar
     </button>
     <div class="link-duplo">
-        <a href="HRegister.html">Não tem uma conta?</a>
+        <a href="HRegister.php">Não tem uma conta?</a>
         <span>|</span>
         <a href="recuperar_senha.html">Esqueceu a senha?</a>
     </div>
